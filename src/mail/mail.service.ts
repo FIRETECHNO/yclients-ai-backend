87bea5f335188e78ddd9c5304b0bdf0f7fd931a1
Poruch <139491864+Poruch@users.noreply.github.com>
2025-08-04 17:38:05 +0500

    add notify teacher registration

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,43 +2,53 @@ import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 
 // types
-import type { User } from 'src/user/interfaces/user.interface'
+import type { User } from 'src/user/interfaces/user.interface';
 
 @Injectable()
 export class MailService {
-  constructor(private mailerService: MailerService) { }
+  constructor(private mailerService: MailerService) {}
 
   public async sendUserConfirmation(user: User) {
     // const url = `example.com/auth/confirm?token=${token}`;
     await this.mailerService.sendMail({
-      to: user.email,
+      to: user.email, //process.env.MAIL_USER,
       from: '"Команда проекта Глазов-есть!" <plpo@ya.ru>', // override default from
       subject: 'Спасибо за регистрацию',
       template: './confirmation', // `.hbs` extension is appended automatically
-      context: { // ✏️ filling curly brackets with content
+      context: {
+        // ✏️ filling curly brackets with content
         name: user.name,
         // url,
       },
     });
   }
-
+  public async sendTeacherRegistrationNotify(user: User) {
+    await this.mailerService.sendMail({
+      to: 'sura14081976@gmail.com',
+      subject: 'Регистрация нового учителя',
+      template: 'teacher-registration-notify',
+      context: {
+        user: user,
+      },
+    });
+  }
   public async sendOrderNotifications(userEmails: string[], order: any) {
     return await this.mailerService.sendMail({
       to: userEmails,
-      from: "Команда Глазов-есть <plpo@ya.ru>", // override default from
+      from: 'Команда Глазов-есть <plpo@ya.ru>', // override default from
       subject: 'Новый заказ',
       template: 'order', // `.hbs` extension is appended automatically
-      context: { order: order._doc }
+      context: { order: order._doc },
     });
   }
 
   public async sendResetLink(link: string, email: string) {
     return await this.mailerService.sendMail({
       to: email,
-      from: "Команда Глазов-есть <plpo@ya.ru>", // override default from
+      from: 'Команда Глазов-есть <plpo@ya.ru>', // override default from
       subject: 'Восстановление пароля',
       template: 'reset-pasword', // `.hbs` extension is appended automatically
-      context: { link }
+      context: { link },
     });
   }
 }
